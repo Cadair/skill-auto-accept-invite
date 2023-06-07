d3ff9e643ee54375bf1c079f68ab8a62c6c3de56
Stuart Mumford <stuart@cadair.com>
2023-06-07 14:30:54 +0100

    Add initial skill framework

---
+++ README.md
@@ -0,0 +1,7 @@
+An opsdroid skill for Automatically Accepting Matrix Invites
+============================================================
+
+This is a trivial skill to allow your bot to accept invites.
+
+Addtionally, it can post in a configured room when it accepts an invite to
+a room to let an admin keep an eye on what rooms it is joining.
---
+++ __init__.py
@@ -0,0 +1,55 @@
+from textwrap import dedent
+
+from opsdroid.connector.matrix import ConnectorMatrix
+from opsdroid.connector.matrix.events import GenericMatrixRoomEvent
+from opsdroid.events import Message, UserInvite
+from opsdroid.matchers import match_regex
+from opsdroid.skill import Skill
+
+INVITEBOT_COMMAND_PREFIX = "!"
+INVITEBOT_COMMANDS = {}
+
+
+def regex_command(command, description="", **kwargs):
+    """
+    A decorator which wraps opsdroid's match_regex to register a command with the !help command.
+    """
+    INVITEBOT_COMMANDS[command] = description
+    def decorator(func):
+        return match_regex(f"^{INVITEBOT_COMMAND_PREFIX}{command}", **kwargs)(func)
+    return decorator
+
+
+@regex_command("help", "print this help message")
+async def help(opsdroid, config, message):
+    commands = "\n".join([f"{INVITEBOT_COMMAND_PREFIX}{command} - {description}" for command, description in INVITEBOT_COMMANDS.items()])
+    help_text = dedent("""\
+    This bot understands the following commands:
+
+    {commands}
+    """).format(commands=commands)
+    await message.respond(help_text)
+
+class AcceptInvite(Skill):
+    """
+    An opsdroid skill to accept invites
+    """
+    @regex_command("rooms", "print a list of all rooms this bot is in.")
+    async def bands(self, message):
+        await message.respond("Not Implemented yet")
+
+    @regex_command("stop", "stop accepting invites")
+    async def stop(self, message):
+        await message.respond("Not Implemented yet.")
+
+    @regex_command("start", "start accepting invites")
+    async def start(self, message):
+        await message.respond("Not Implemented yet.")
+
+    @match_event(UserInvite)
+    async def on_invite_to_room(self, invite):
+        """
+        Join all rooms on invite.
+        """
+        _LOGGER.info("Got room invite.")
+        await invite.respond(JoinRoom())
