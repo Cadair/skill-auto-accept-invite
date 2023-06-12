2d3ca940437e43a4a570ac0d698e3c3908d0bfed
Stuart Mumford <stuart@cadair.com>
2023-06-12 10:50:52 +0100

    Only use commands in special room

--- __init__.py
+++ __init__.py
@@ -23,31 +23,76 @@ def regex_command(command, description="", **kwargs):
     return decorator
 
 
-@regex_command("help", "print this help message")
-async def help(opsdroid, config, message):
-    commands = "\n".join([f"{INVITEBOT_COMMAND_PREFIX}{command} - {description}" for command, description in INVITEBOT_COMMANDS.items()])
-    help_text = dedent("""\
-    This bot understands the following commands:
-
-    {commands}
-    """).format(commands=commands)
-    await message.respond(help_text)
 
 class AcceptInvite(Skill):
     """
     An opsdroid skill to accept invites
     """
+    def __init__(self, opsdroid, config):
+        super().__init__(opsdroid, config)
+        self._auto_invite = True
+        self._admin_room = None
+
+    @property
+    def matrix_connector(self):
+        matrix_connector = [c for c in self.opsdroid.connectors if isinstance(c, ConnectorMatrix)]
+        if not matrix_connector:
+            raise ValueError("The auto-accept-invite skill only works with the matrix connector")
+        return matrix_connector[0]
+
+    async def auto_invite(self):
+        if self._auto_invite is not None:
+            return self._auto_invite
+
+        # TODO: Get from database in admin room
+
+    async def admin_room_id(self):
+        """
+        Get the room_id of the admin room.
+        """
+        if self._admin_room is not None:
+            return self._admin_room
+
+        admin_room = self.matrix_connector.config["rooms"].get("auto-accept-invite")
+        if admin_room is not None and not admin_room.startswith("!"):
+            response = await self.connection.room_resolve_alias(admin_room)
+            if isinstance(response, nio.RoomResolveAliasError):
+                _LOGGER.error(
+                    f"Error resolving room id for {self.admin_room}: {response.message} (status code {response.status_code})"
+                )
+            else:
+                admin_room = response.room_id
+        self._admin_room = admin_room
+
     @regex_command("rooms", "print a list of all rooms this bot is in.")
     async def bands(self, message):
+        if message.target != await self.admin_room_id():
+            return
         await message.respond("Not Implemented yet")
 
     @regex_command("stop", "stop accepting invites")
     async def stop(self, message):
+        if message.target != await self.admin_room_id():
+            return
         await message.respond("Not Implemented yet.")
 
     @regex_command("start", "start accepting invites")
     async def start(self, message):
+        if message.target != await self.admin_room_id():
+            return
         await message.respond("Not Implemented yet.")
+
+    @regex_command("help", "print this help message")
+    async def help(self, message):
+        if message.target != await self.admin_room_id():
+            return
+        commands = "\n".join([f"{INVITEBOT_COMMAND_PREFIX}{command} - {description}" for command, description in INVITEBOT_COMMANDS.items()])
+        help_text = dedent("""\
+        This bot understands the following commands:
+
+        {commands}
+        """).format(commands=commands)
+        await message.respond(help_text)
 
     @match_event(UserInvite)
     async def on_invite_to_room(self, invite):
@@ -55,4 +100,8 @@ class AcceptInvite(Skill):
         Join all rooms on invite.
         """
         _LOGGER.info("Got room invite.")
-        await invite.respond(JoinRoom())
+        if await self.auto_invite():
+            await invite.respond(JoinRoom())
+            _LOGGER.info("Accepted room invite.")
+        else:
+            _LOGGER.info("Rejected room invite because auto invite is off.")
